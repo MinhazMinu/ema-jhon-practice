250ffe0c5f5c45d86b6e2d41dc9f1cf6141e64a6
minhaz <minhaz.taher@gmail.com>
2020-03-16 15:11:45 +0600

    module1 done

--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -2,11 +2,19 @@ import React from "react";
 
 const Cart = ({ cart }) => {
   console.log(cart);
-
+  const Total = cart.reduce((sum, i) => {
+    return (sum += i.price);
+  }, 0);
+  const shipping = cart.length ? (Total > 350 ? 0 : 40.95) : 0;
+  const tex = (Total * 0.15).toFixed(2);
+  const grandtotal = (Total + shipping).toFixed(2);
   return (
     <div>
       <h4>Order Summery</h4>
       <h5>Item Ordered : {cart.length}</h5>
+      <h5>Shiping Cost :{shipping}</h5>
+      <h5>Tex + Vat : {tex}</h5>
+      <h5>Total Price: {parseFloat(grandtotal)}</h5>
     </div>
   );
 };
