44759e93d2c1c8fbefc40d5b7a9a433329309948
minhaz <minhaz.taher@gmail.com>
2020-03-16 12:37:40 +0600

    display more data from product

--- src/component/shop/Shop.js
+++ src/component/shop/Shop.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import fakeData from "../../fakeData";
+import "./Shop.css";
+import Product from "../product/Product";
 
 const Shop = () => {
   /**
@@ -9,12 +11,17 @@ const Shop = () => {
   const [products, setProducts] = useState(first10);
 
   return (
-    <div>
-      <ul>
-        {products.map(item => (
-          <li> {item.name}</li>
-        ))}
-      </ul>
+    <div className="shop-container">
+      <div className="product-container">
+        <ul>
+          {products.map(product => (
+            <Product product={product}> </Product>
+          ))}
+        </ul>
+      </div>
+      <div className="cart-container">
+        <h2>cart</h2>
+      </div>
     </div>
   );
 };
